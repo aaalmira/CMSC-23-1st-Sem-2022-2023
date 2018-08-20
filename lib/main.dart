db8f61f6b074c42ed6d832da73387716d2d386e5
almira <aaalmira@up.edu.ph>
2018-08-20 20:19:19 +0100

    sa

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+/*
+    Author: Anton Almira
+    Section: C-1L
+    Date Created: October 17, 2022
+    Exercise Number: 5
+    Program Description: A simple to-do list.
+
+*/
+
+import 'package:flutter/material.dart';
+
+//import necessary packages
+import 'show_dogs.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Dogs List';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      theme: ThemeData(
+        primarySwatch: Colors.green
+      ),
+      home: ShowDogsPage(),
+    );
+  }
+}
